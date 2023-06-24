f3f923f4bf5698f398733e0ee47c45b0b01e9b80
barakatimothy <timothybaraka39@gmail.com>
2023-06-24 14:21:34 -0400

    complete!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 let addToy = false;
-
+const URL = `http://localhost:3000/toys`
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -13,3 +13,78 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+fetchToy()
+document.querySelector('add-toy-form').addEventListener('submit',
+(e) =>{ 
+  
+  e.preventDefault()
+  let newToy = {
+    'name': e.target.name.value,
+    'image':e.target.name.value,
+     'likes': 0,
+  }
+  postToy(newToy)
+}
+)
+function postToy(newToy){
+  let options = {
+    method: 'POST',
+    headers :{
+      'content-type':'application/json',
+      'Accept':'application/json'
+    },
+    body: JSON.stringify(newToy)
+  }
+  fetch (url, options)
+  .then(res => res.json())
+  .then( renderCard)
+}
+function fetchToys(){
+  fetch(URL)
+  .then(res => res.json())
+  .then(toyArr => toyArr.forEach(toy => renderCard(toy)))
+}
+function addLIkes(){
+  console.log(toy,'before patch')
+  likesCount = parseInt(likes.innerText)+1;
+  let newLikes ={
+    'likes': likesCount
+  }
+  let options ={
+    method : 'PATCH',
+    headers: {
+      'content-type': 'application/json',
+      'Accept':'application/json'
+    },
+    body: JSON.stringify(newLikes)
+  }
+  fetch(URL+ toy.id, options)
+  .then(res=>res.json())
+  .then(updatedToy => likes.innerText = `${updatedToy.likes} Likes`)
+}
+function renderCard(toy){
+  const toyCollection = document.getElementById('toy-collection')
+
+  let toyCard = document.createElement('div')
+  toyCard.className = 'card'
+
+  let toyName = document.createElement('h2')
+  toyName.innerText = toy.name
+
+  let toyImg = document.createElement('img')
+  toyImg.className = 'toy-avatar'
+  toyImg.src = toy.image
+
+  let likes = document.createElement('p')
+  likes.innerText = `${toy.likes} Likes`
+
+  let likeButton = document.createElement('button')
+  likeButton.className = "like-btn"
+  likeButton.innerText = "Like"
+  likeButton.addEventListener('click', (e)=>{
+    addLikes(toy, likes)
+  })
+
+  toyCard.append(toyName, toyImg, likes, likeButton)
+  toyCollection.append(toyCard)
+}
